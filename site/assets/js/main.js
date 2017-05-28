44a55c59db4d792613bbe867bfdda069ed28e16d
konstantinbudasov <k.budasov@gmail.com>
2017-05-28 12:38:41 +0300

    Updated Profile and settings, adjusted form validation, added modals, few new pages

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -5,9 +5,27 @@ var commonActions = (function (document, $) {
     var $doc = $(document);
 
     var selector = {
+        form: {
+            element: '.js-form-validate',
+            changeManager: '#form-reason-to-change-manager'
+        },
+        modal: {
+            changeManager: '#modal-change-personal-manager',
+            newManager: '#modal-new-personal-manager'
+        },
+
+        chat: {
+            startManagerChat: '.js-start-manager-chat',
+            managerArea: '#chat-manager-area',
+            messagesThread: '#chat-thread',
+            textarea: '#chat-textarea'
+        },
         slidingMenuItem: '.sliding-menu__item',
         slidingMenuTitle: '.js-sliding-menu-title',
-        nextElToggler: '.js-toggle-next-el'
+        nextElToggler: '.js-toggle-next-el',
+        openModal: '.js-open-modal',
+        closeModal: '.js-close-modal'
+
     };
 
     var className = {
@@ -15,6 +33,77 @@ var commonActions = (function (document, $) {
         visible:  'is-visible',
         hover:    'is-hover'
     };
+
+    var $form = $(selector.form.element);
+
+    $.each($form, function(){
+        var $form = $(this);
+
+        $form.parsley()
+            .on('form:error', _focusOnFirstFieldError)
+            .on('form:submit', _onFormSubmit);
+    });
+
+    $(selector.form.changeManager).parsley().on('form:submit', function(){
+        $(selector.modal.changeManager).modal('hide');
+
+        setTimeout(function(){
+            $(selector.modal.newManager).modal('show');
+        }, 200);
+
+        return false;
+    });
+
+    function _startManagerChat() {
+        $(selector.chat.startManagerChat).on('click', function(){
+            $(selector.modal.newManager).modal('hide');
+
+            $('html, body').animate({
+                scrollTop: $(selector.chat.managerArea).offset().top
+            }, 250);
+        });
+
+        $(selector.chat.messagesThread).animate({
+           scrollTop: $(selector.chat.messagesThread)[0].scrollHeight
+        }, 0);
+
+        $(selector.chat.textarea).focus();
+    }
+
+    function _onFormSubmit(e) {
+        if(e.validationResult = false) {
+            return false;
+        }
+    }
+
+    function _focusOnFirstFieldError() {
+        var $form = this.$element;
+        var $firstInvalidField = $form.find('.parsley-error:first');
+        var errors = $firstInvalidField.parsley().getErrorsMessages().join(';');
+
+        //if (!!errors) _showError(errors, $firstInvalidField);
+    }
+
+    function _bindModals() {
+        $(selector.openModal).on('click', function(){
+            var $targetId = $(this).attr('data-target-id');
+            var $modalSelector = '#' + $targetId;
+            var $modalEl = $($modalSelector);
+
+            $modalEl.on('shown.bs.modal', function () {
+                var $modalFormInputs = $modalEl.find('.form-control');
+
+                if($modalFormInputs.length) {
+                    $modalFormInputs[0].focus();
+                }
+            });
+
+            if($modalEl.length) {
+                $modalEl.modal('show');
+            }
+
+        });
+    }
 
     function _toggleNextEl() {
         $(selector.nextElToggler).on('click', function(){
@@ -58,11 +147,14 @@ var commonActions = (function (document, $) {
         });
     }
 
-    function bind() {
+    function _onDocumentClick() {
 
-        $('#chat-thread').animate(
-            {
-            scrollTop: $('#chat-thread')[0].scrollHeight}, 0);
+    }
+
+    function bind() {
+        _bindModals();
+
+        _startManagerChat();
 
         _toggleNextEl();
 
@@ -78,8 +170,8 @@ var commonActions = (function (document, $) {
         //     cache : {}
         // }
 
-        $('input[name="password"]').passwordStrength();
-        $('input[name="password2"]').passwordStrength({targetDiv: '#password-strength-scale',classes : Array('is10','is20','is30','is40')});
+        //$('input[name="password"]').passwordStrength();
+        //$('input[name="password2"]').passwordStrength({targetDiv: '#password-strength-scale',classes : Array('is10','is20','is30','is40')});
 
     }
 
