01f1740b926ff69cb90c791c3db4ba25b74aded2
BarnabyJ <vlaschenko.michael@gmail.com>
2017-07-12 18:00:45 +0300

    Added design media page and pages with dynamic charts.

--- site/assets/js/vendors/charts.js
+++ site/assets/js/vendors/charts.js
@@ -121,8 +121,8 @@ $(function () {
             name: 'Browser share',
             innerSize: 220,
             data: [
-                ['Ингосстрах. Облигации',   59.69],
-                ['Ингосстрах. Еврооблигации',       32.03],
+                ['Ингосстрах. Облигации', 59.69],
+                ['Ингосстрах. Еврооблигации', 32.03],
                 ['Ингосстрах. Риэл Эстейт', 8.28],
             ],
             states: {
@@ -166,7 +166,7 @@ $(function () {
             name: 'Browser share',
             innerSize: 220,
             data: [
-                ['Облигации',   86.78],
+                ['Облигации', 86.78],
                 ['Прочие вложения', 12.21],
                 ['Денежные средства у брокера', 1.01],
             ],
@@ -211,7 +211,7 @@ $(function () {
             name: 'Browser share',
             innerSize: 220,
             data: [
-                ['Линейная P&G',   47.43],
+                ['Линейная P&G', 47.43],
                 ['ИИС Рыночная', 25.49],
                 ['Линейная Apple', 19.91],
                 ['ИИС Рыночная', 4.38],
@@ -302,9 +302,11 @@ $(function () {
                 x: -360,
                 useHTML: true,
                 enabled: true,
-                formatter: function() { return '<div class="charts-column__data">'
-                    + '<div class="charts-column__data-item">' + industryData[this.value][0] + '</div>'
-                    + '<div class="charts-column__data-item _count">' + industryData[this.value][1] + '</div></div>';}
+                formatter: function () {
+                    return '<div class="charts-column__data">'
+                        + '<div class="charts-column__data-item">' + industryData[this.value][0] + '</div>'
+                        + '<div class="charts-column__data-item _count">' + industryData[this.value][1] + '</div></div>';
+                }
             }
         },
         yAxis: {
@@ -353,9 +355,11 @@ $(function () {
                 x: -360,
                 useHTML: true,
                 enabled: true,
-                formatter: function() { return '<div class="charts-column__data">'
-                    + '<div class="charts-column__data-item">' + assetsClassesData[this.value][0] + '</div>'
-                    + '<div class="charts-column__data-item _count">' + assetsClassesData[this.value][1] + '</div></div>';}
+                formatter: function () {
+                    return '<div class="charts-column__data">'
+                        + '<div class="charts-column__data-item">' + assetsClassesData[this.value][0] + '</div>'
+                        + '<div class="charts-column__data-item _count">' + assetsClassesData[this.value][1] + '</div></div>';
+                }
             }
         },
         yAxis: {
@@ -409,9 +413,11 @@ $(function () {
                 x: -360,
                 useHTML: true,
                 enabled: true,
-                formatter: function() { return '<div class="charts-column__data">'
-                    + '<div class="charts-column__data-item">' + currencyData[this.value][0] + '</div>'
-                    + '<div class="charts-column__data-item _count">' + currencyData[this.value][1] + '</div></div>';}
+                formatter: function () {
+                    return '<div class="charts-column__data">'
+                        + '<div class="charts-column__data-item">' + currencyData[this.value][0] + '</div>'
+                        + '<div class="charts-column__data-item _count">' + currencyData[this.value][1] + '</div></div>';
+                }
             }
         },
         yAxis: {
@@ -470,9 +476,11 @@ $(function () {
                 x: -360,
                 useHTML: true,
                 enabled: true,
-                formatter: function() { return '<div class="charts-column__data">'
-                                            + '<div class="charts-column__data-item">' + issuersData[this.value][0] + '</div>'
-                                            + '<div class="charts-column__data-item _count">' + issuersData[this.value][1] + '</div></div>';}
+                formatter: function () {
+                    return '<div class="charts-column__data">'
+                        + '<div class="charts-column__data-item">' + issuersData[this.value][0] + '</div>'
+                        + '<div class="charts-column__data-item _count">' + issuersData[this.value][1] + '</div></div>';
+                }
             }
         },
         yAxis: {
@@ -502,101 +510,190 @@ $(function () {
 
     });
 
+    if(Highcharts.stockChart){
+        $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
 
-    // Load the data from a Google Spreadsheet
-    // https://docs.google.com/a/highsoft.com/spreadsheet/pub?hl=en_GB&hl=en_GB&key=0AoIaUO7wH1HwdFJHaFI4eUJDYlVna3k5TlpuXzZubHc&output=html
-    Highcharts.data({
+            // Create the chart
+            var chart = Highcharts.stockChart('history-chart', {
 
-        googleSpreadsheetKey: '0AoIaUO7wH1HwdFJHaFI4eUJDYlVna3k5TlpuXzZubHc',
-
-        // custom handler when the spreadsheet is parsed
-        parsed: function (columns) {
-
-            // Read the columns into the data array
-            var data = [];
-            $.each(columns[0], function (i, code) {
-                data.push({
-                    code: code.toUpperCase(),
-                    value: parseFloat(columns[2][i]),
-                    name: columns[1][i]
-                });
-            });
-
-            // Initiate the chart
-            $('#geography-chart').highcharts('Map', {
                 chart: {
-                    spacingLeft: -10,
-                    spacingRight: -10,
-                    height: 450
+                    height: 400
                 },
                 title: false,
-                mapNavigation: {
+                rangeSelector: {
                     enabled: false
                 },
-                legend: {
-                    align: 'left',
-                    verticalAlign: 'bottom',
-                    floating: true,
-                    layout: 'vertical',
-                    valueDecimals: 0,
-                    itemMarginTop: 18,
-                    symbolRadius: 0,
-                    symbolWidth: 35,
-                    symbolHeight: 30,
-                    useHTML: true,
-                    y: 20
+                xAxis: {
+                    tickWidth: 0
                 },
-
-                colorAxis: {
-                    dataClasses: [{
-                        from: 0,
-                        to: 3,
-                        color: '#b9eafc'
-                    }, {
-                        from: 3,
-                        to: 10,
-                        color: '#acdff6'
-                    }, {
-                        from: 10,
-                        to: 30,
-                        color: '#74cdf6'
-                    }, {
-                        from: 30,
-                        to: 100,
-                        color: '#44c4f9'
-                    }, {
-                        from: 100,
-                        to: 300,
-                        color: '#30b8f6'
-                    }, {
-                        from: 300,
-                        to: 1500,
-                        color: '#00adeb'
-                    }]
+                yAxis: {
+                    gridLineWidth: 0,
+                    labels: {
+                        enabled: false
+                    }
                 },
-
-                series: [{
-                    data: data,
-                    mapData: Highcharts.maps['custom/world'],
-                    joinBy: ['iso-a2', 'code'],
-                    animation: true,
-                    name: 'ЗАНЧЕНИЕ',
-                    color: '#b9eafc',
-                    borderColor: '#ffffff',
-                    states: {
-                        hover: {
-                            color: '#00adeb'
+                plotOptions: {
+                    series: {
+                        marker: {
+                            fillColor: '#FFFFFF',
+                            lineWidth: 1,
+                            lineColor: '#00adeb',
+                            symbol: 'circle',
+                            states: {
+                                hover: {
+                                    lineWidth: 3,
+                                    radius: 6
+                                }
+                            }
+                        },
+                        fillOpacity: 1,
+                        lineColor: '#00adeb',
+                        lineWidth: 2,
+                        states: {
+                            hover: {
+                                lineWidth: 3
+                            }
                         }
                     }
+                },
+                scrollbar: {
+                    barBackgroundColor: '#9ea3a9',
+                    barBorderWidth: 0,
+                    buttonBackgroundColor: '#9ea3a9',
+                    buttonBorderWidth: 0,
+                    buttonArrowColor: '#e1e1e1',
+                    rifleColor: '#e1e1e1',
+                    trackBackgroundColor: '#f1f2f2',
+                    trackBorderWidth: 1,
+                    trackBorderColor: '#dcdedf'
+                },
+                navigator: {
+                    series: {
+                        color: '#f9f9fa',
+                        lineColor: '#dcdedf',
+                        lineWidth: 1
+                    },
+                    maskFill: 'rgba(204, 239, 251, 0.75)'
+                },
+                tooltip: {
+                    useHTML: true,
+                    crosshairs: false,
+                    formatter: function () {
+                        return '<div class="tooltip-container">'
+                            + '<div class="tooltip-col"><span class="tooltip-title">ДАТА:</span>'
+                            + '<span class="tooltip-value">' + Highcharts.dateFormat('%e %b %Y', new Date(this.x)) + '</span></div>'
+                            + '<div class="tooltip-col"><span class="tooltip-title">ИЗМЕНЕНИЕ:</span>'
+                            + '<span class="tooltip-value _green">' + Highcharts.numberFormat(this.y, 2) + ' % </span></div></div></div>';
+                    }
+                },
+                series: [{
+                    data: data,
+                    type: 'area',
+                    color: '#cceffb',
+                    threshold: null,
                 }]
+
             });
-        },
-        error: function () {
-            $('#geography-chart').html('<div class="loading">' +
-                '<i class="icon-frown icon-large"></i> ' +
-                'Проблемы с загрузкой данных' +
-                '</div>');
-        }
-    });
+        });
+
+    }
+
+    if(Highcharts.data){
+        // Load the data from a Google Spreadsheet
+        // https://docs.google.com/a/highsoft.com/spreadsheet/pub?hl=en_GB&hl=en_GB&key=0AoIaUO7wH1HwdFJHaFI4eUJDYlVna3k5TlpuXzZubHc&output=html
+        Highcharts.data({
+
+            googleSpreadsheetKey: '0AoIaUO7wH1HwdFJHaFI4eUJDYlVna3k5TlpuXzZubHc',
+
+            // custom handler when the spreadsheet is parsed
+            parsed: function (columns) {
+
+                // Read the columns into the data array
+                var data = [];
+                $.each(columns[0], function (i, code) {
+                    data.push({
+                        code: code.toUpperCase(),
+                        value: parseFloat(columns[2][i]),
+                        name: columns[1][i]
+                    });
+                });
+
+                // Initiate the chart
+                $('#geography-chart').highcharts('Map', {
+                    chart: {
+                        spacingLeft: -10,
+                        spacingRight: -10,
+                        height: 450
+                    },
+                    title: false,
+                    mapNavigation: {
+                        enabled: false
+                    },
+                    legend: {
+                        align: 'left',
+                        verticalAlign: 'bottom',
+                        floating: true,
+                        layout: 'vertical',
+                        valueDecimals: 0,
+                        itemMarginTop: 18,
+                        symbolRadius: 0,
+                        symbolWidth: 35,
+                        symbolHeight: 30,
+                        useHTML: true,
+                        y: 20
+                    },
+
+                    colorAxis: {
+                        dataClasses: [{
+                            from: 0,
+                            to: 3,
+                            color: '#b9eafc'
+                        }, {
+                            from: 3,
+                            to: 10,
+                            color: '#acdff6'
+                        }, {
+                            from: 10,
+                            to: 30,
+                            color: '#74cdf6'
+                        }, {
+                            from: 30,
+                            to: 100,
+                            color: '#44c4f9'
+                        }, {
+                            from: 100,
+                            to: 300,
+                            color: '#30b8f6'
+                        }, {
+                            from: 300,
+                            to: 1500,
+                            color: '#00adeb'
+                        }]
+                    },
+
+                    series: [{
+                        data: data,
+                        mapData: Highcharts.maps['custom/world'],
+                        joinBy: ['iso-a2', 'code'],
+                        animation: true,
+                        name: 'ЗАНЧЕНИЕ',
+                        color: '#b9eafc',
+                        borderColor: '#ffffff',
+                        states: {
+                            hover: {
+                                color: '#00adeb'
+                            }
+                        }
+                    }]
+                });
+            },
+            error: function () {
+                $('#geography-chart').html('<div class="loading">' +
+                    '<i class="icon-frown icon-large"></i> ' +
+                    'Проблемы с загрузкой данных' +
+                    '</div>');
+            }
+        });
+    }
 
 })
